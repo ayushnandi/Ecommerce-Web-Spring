0f6b79885e4cbedd3de5014ae0399dd49880b992
Sunny955 <sunnyranjan9050@gmail.com>
2025-01-23 21:31:37 +0530

    Improved the code for product class

--- src/main/java/com/ayush/ecom_proj/model/Product.java
+++ src/main/java/com/ayush/ecom_proj/model/Product.java
@@ -1,5 +1,6 @@
 package com.ayush.ecom_proj.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,13 +20,21 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String brand;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date releaseDate;
+    @JsonFormat(shape = JsonFormat.Shape.BOOLEAN)
     private boolean available;
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
     private int quantity;
 }
 
